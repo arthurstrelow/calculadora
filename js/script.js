e7c6be69ff37e3f9bb985ec6f0d973da0821ca7b
Arthur <106569079+arthurstrelow@users.noreply.github.com>
2023-03-14 12:51:45 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,38 @@
+let display = document.querySelector('#display')
+let botoes = document.querySelectorAll('button')
+var reseta = false
+var ultimocalculo = 0
+botoes.forEach((i) => {
+    let valor = i.id
+    i.addEventListener('click', () => {
+        switch (valor){
+            case '=':
+                try{
+                    let calculo = eval(display.value)
+                    //Não recomendo o uso do "eval()", como o projeto é local e muito simples foi usado
+                    //Portanto, para projeto maiores e querer ter uma calculadora, usasse a lib "Math.js"
+                    display.value = calculo
+                    ultimocalculo = calculo
+                }catch (e){
+                    display.value = 'ERR!'
+                }
+                reseta = true
+                break
+            case 'ac':
+                display.value = ''
+                reseta = false
+                break
+            case 'de':
+                display.value = display.value.slice(0, display.value.length - 1)
+                reseta = false
+                break
+            default:
+                if(reseta){
+                    display.value = ultimocalculo
+                    reseta = false
+                }
+                display.value += valor
+                break
+        }
+    })
+})
